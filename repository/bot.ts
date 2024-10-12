9adf8b7df0bb617fe383ce2a5a7338a1cf6ac097
CEERG <ceerg@yandex.ru>
2024-10-12 15:31:17 +0500

    graceful shutdown

--- repository/bot.ts
+++ repository/bot.ts
@@ -1,19 +1,5 @@
 import Bot from './src/bot/Bot';
 
-process.stdin.resume();
-
-['SIGINT', 'SIGTERM', 'SIGQUIT']
-  .forEach(signal => process.on(signal, () => {
-    console.log(`AAAAAAAAAAAAAAAAAA ${signal}`);
-    process.exit();
-  }));
-
-process.on( 'SIGTERM', function() {
-  bot.stop();
-
-  process.exit();
-});
-
 const bot = new Bot();
 
 bot.start();
--- repository/data_collector.ts
+++ repository/data_collector.ts
@@ -1,9 +1,5 @@
 import DataCollector from "./src/data_collector/DataCollector";
 
-process.on( 'SIGTERM', () => {
-  process.exit();
-});
-
 const dataCollector = new DataCollector;
 
 dataCollector.start();
